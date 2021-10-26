12afaae6eceec85035c33a139cb67480d4b794a0
Bianca Fontes <86249549+rocketoof@users.noreply.github.com>
2021-10-25 21:09:09 -0300

    final 2 de out

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
-const config = require("./config.json")
+const { Client, Intents } = require('discord.js')
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+})
+const config = require('./config.json')
 
 client.login(config.token)
