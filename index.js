4e7799e66aea5e5a1a285cd5ba2f6553ff12df70
Bianca Fontes <86249549+rocketoof@users.noreply.github.com>
2021-10-26 13:20:31 -0300

    deleted say.js

--- index.js
+++ index.js
@@ -5,27 +5,7 @@ const client = new Client({
 const config = require('./config.json')
 
 client.on('ready', () => {
-  console.log("Estou online!!!")
+  console.log('Estou online!!!')
 })
-
-ClientPresence.on("message", async message => {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-  if (!message.content.startsWith(config.prefix)) return;
-  if (message.content.startsWith(`<@!${cliente.user.id}`) || message.content.startsWith(`<@${cliente.user.id}`)) return;
-
-  let args = message.content.split(" ").slice(1);
-  let command = message.content.split(" ")[0];
-  command = command.slice(config.prefix.length);
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    delete require.cache[require.resolve(`./command/${command}.js`)];
-    return commandFile.run(client, message, args);    
-  } 
-  catch (err){
-    console.error("Erro:" + err);
-
-  }
-});
 
 client.login(config.token)
